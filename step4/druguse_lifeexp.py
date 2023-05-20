35589ea87f4fb755f25a9f62bcb8316fd37a59aa
Cansın Narşahin <79102795+cansnnnn@users.noreply.github.com>
2023-05-20 18:48:43 +0300

    Update druguse_lifeexp.py

--- step4/druguse_lifeexp.py
+++ step4/druguse_lifeexp.py
@@ -4,7 +4,7 @@ import pandas as pd
 import mysql.connector
 
 cnx = mysql.connector.connect(user='root',
-                              password='',
+                              password='password',
                               host='localhost',
                               database='project')
 
@@ -20,17 +20,41 @@ cnx.close()
 
 data = pd.DataFrame(rows, columns=['C_Code', 'YYear', 'drugdeaths', 'Life_ex_value', 'Country_Name'])
 
-# filter data for years between 2010 and 2023
-filtered_data = data[(data['YYear'] >= 2010) & (data['YYear'] <= 2023)]
+# filter data for years between 1990 and 2012
+filtered_data = data[(data['YYear'] >= 1990) & (data['YYear'] <= 2012)]
 
+#take ratio off drug deaths an life exp
 filtered_data['Ratio'] = filtered_data['drugdeaths'] / filtered_data['Life_ex_value']
 
-plt.scatter(filtered_data['Country_Name'], filtered_data['Ratio'], c=filtered_data['YYear'], cmap='viridis')
+
+filtered_data['YYear'] = filtered_data['YYear'].astype(int)
+
+plt.figure(figsize=(8, 8))  # Adjust the width and height as needed
+
+scatter = plt.scatter(filtered_data['Country_Name'], filtered_data['Ratio'], c=filtered_data['YYear'], cmap='viridis')
 plt.xticks(rotation='vertical')
 plt.xlabel('Country')
 plt.ylabel('Ratio (Drug Deaths / Life Expectancy)')
 plt.title('Ratio of Drug Deaths to Life Expectancy for high_drugdeaths_low_lifeex Countries')
-plt.colorbar(label='Year')
+
+cbar = plt.colorbar(scatter, label='Year')
+cbar.set_ticks(filtered_data['YYear'].unique())
+
+countries = filtered_data['Country_Name'].unique()
+x_ticks = np.arange(len(countries))
+x_labels = [country for country in countries]
+
+#for dates in labels
+for country in countries:
+    country_data = filtered_data[filtered_data['Country_Name'] == country]
+    first_year = country_data['YYear'].min()
+    last_year = country_data['YYear'].max()
+    date_label = f"{first_year}-{last_year}"
+    x_idx = np.where(np.array(x_labels) == country)[0]
+    if len(x_idx) > 0:
+        x_labels[x_idx[0]] = f"{country}\n({date_label})"
+
+plt.xticks(x_ticks, x_labels)
 
 plt.tight_layout()
 plt.show()
